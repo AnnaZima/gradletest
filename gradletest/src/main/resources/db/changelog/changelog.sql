071354c031fe2e9211c90e0f011057ac3ecc15b9
Anna Kat <annasobolevskaya2017@gmail.com>
2023-10-25 16:15:45 +0300

    test commit

--- gradletest/src/main/resources/db/changelog/changelog.sql
+++ gradletest/src/main/resources/db/changelog/changelog.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 
 -- changeset liquibase:1
-CREATE TABLE specialty (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    spec_name VARCHAR(255) NOT NULL);
+CREATE TABLE test (
+    test_id INT AUTO_INCREMENT PRIMARY KEY,
+    test_name VARCHAR(255) NOT NULL);
